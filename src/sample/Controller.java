4c1b8bbdc9658bd3984ca4ad1781d84e44038a98
Logres <logres_king@126.com>
2021-10-15 09:40:18 +0800

    UI设计与接口预留

---
+++ src/sample/Controller.java
@@ -0,0 +1,206 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+
+        @FXML
+        private TextField screenMain;
+
+        @FXML
+        private Button butPlusMinus;
+
+        @FXML
+        private Button but0;
+
+        @FXML
+        private Button butDot;
+
+        @FXML
+        private Button butEqual;
+
+        @FXML
+        private Button but2;
+
+        @FXML
+        private Button but1;
+
+        @FXML
+        private Button but3;
+
+        @FXML
+        private Button butPlus;
+
+        @FXML
+        private Button but4;
+
+        @FXML
+        private Button but5;
+
+        @FXML
+        private Button but6;
+
+        @FXML
+        private Button butMinus;
+
+        @FXML
+        private Button butBackspace;
+
+        @FXML
+        private Button but7;
+
+        @FXML
+        private Button but8;
+
+        @FXML
+        private Button but9;
+
+        @FXML
+        private Button butMul;
+
+        @FXML
+        private Button butPercent;
+
+        @FXML
+        private Button butC;
+
+        @FXML
+        private Button butFrac;
+
+        @FXML
+        private Button butCE;
+
+        @FXML
+        private Button butSquare;
+
+        @FXML
+        private Button butRoot;
+
+        @FXML
+        private Button butDiv;
+
+        @FXML
+        void but0Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but1Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but2handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but3Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but4Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but5Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but6Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but7Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but8Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void but9Handler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butBackspaceHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butCEHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butCHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butDivHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butDotHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butEqualHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butFracHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butMinusHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butMulHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butPercentHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butPlusHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butPlusMinusHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butRootHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void butSquareHandler(ActionEvent event) {
+
+        }
+
+}
